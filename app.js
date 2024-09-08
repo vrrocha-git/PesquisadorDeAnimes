8993e60619a72f6f81b76a865489792569bf4254
vrrocha-git <47858174+vrrocha-git@users.noreply.github.com>
2024-09-07 23:53:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+import { dados } from "./dados.js";
+
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    // Verifica se a pesquisa tem apenas um caractere e se é um espaço em branco
+    if (campoPesquisa.length == 3 || campoPesquisa === " ") {
+        section.innerHTML = `<p class="descricao-meta">Pesquisa inválida: por favor, insira mais caracteres.</p>`;
+        return;
+    }
+
+    if (campoPesquisa === "") {
+        section.innerHTML = `<p class="descricao-meta">Nenhum resultado encontrado</p>`;
+        return;
+    }
+
+    let resultados = "";
+    let descricao = "";
+    let titulo = "";
+    let genero = "";
+    let criador = "";
+
+    for (let dado of dados){
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        genero = dado.genero.toLowerCase()
+        criador = dado.criador.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || genero.includes(campoPesquisa) || criador.includes(campoPesquisa)){
+            //cria um novo elemento
+            resultados+=`
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <p class="descricao-meta"><b>Principais personagens: </b>${dado.principaisPersonagens}</p>
+                <p class="descricao-meta"><b>Criador: </b>${dado.criador}</p>
+                <p class="descricao-meta"><b>Gênero: </b>${dado.genero}</p>
+                <p class="descricao-meta"><b>Ano de Lançamento: </b>${dado.anoLancamento}</p>
+                <p class="descricao-meta"><b>Número de Episódios: </b>${dado.numEpisodios}</p>
+                <p class="descricao-meta"><b>Estúdio: </b>${dado.estudio}</p>
+
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+            `
+        }
+        
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    section.innerHTML = resultados;
+};
+
+// Obtém o botão e associa a função ao evento de clique
+const botaoPesquisar = document.getElementById('botao-pesquisar');
+botaoPesquisar.addEventListener('click', pesquisar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,137 @@
+export let dados = [
+        {
+          titulo: "Naruto",
+          descricao: "Naruto Uzumaki é um jovem ninja que busca reconhecimento e sonha em se tornar Hokage, o líder da vila de Konoha. Acompanhe as aventuras de Naruto enquanto ele enfrenta desafios, faz amigos e descobre os segredos de seu passado. A série aborda temas como amizade, superação e o significado de ser um ninja.",
+          link: "https://pt.wikipedia.org/wiki/Naruto",
+          principaisPersonagens: "Naruto Uzumaki, Sasuke Uchiha, Sakura Haruno",
+          criador: "Masashi Kishimoto",
+          genero: "Ação, Shounen",
+          anoLancamento: "2002",
+          estudio: "Studio Pierrot",
+          numEpisodios: "720",
+          imagem: "https://example.com/naruto.jpg"
+        },
+        {
+          titulo: "Dragon Ball",
+          descricao: "Dragon Ball é uma franquia de anime e mangá japonesa criada por Akira Toriyama que acompanha as aventuras de Son Goku, um jovem com rabo de macaco que busca as Esferas do Dragão, artefatos místicos capazes de realizar qualquer desejo. Ao longo da série, Goku e seus amigos enfrentam diversos vilões e treinam para se tornarem os guerreiros mais fortes do universo, enquanto exploram temas como amizade, lealdade e superação. A saga, que se tornou um fenômeno mundial, é conhecida por suas lutas épicas, personagens carismáticos e humor irreverente.",
+          link: "https://pt.wikipedia.org/wiki/Dragon_Ball_(s%C3%A9rie)",
+          principaisPersonagens: "Goku, Vegeta, Piccolo, Gohan, Bulma",
+          criador: "Akira Toriyama",
+          genero: "Ação, Aventura, Comédia",
+          anoLancamento: "1986",
+          estudio: "Toei Animation",
+          numEpisodios: "153 + filmes",
+          imagem: "https://example.com/dragon_ball.jpg"
+        },
+        {
+          titulo: "One Piece",
+          descricao: "Em um mundo de piratas, Monkey D. Luffy embarca em uma jornada épica em busca do One Piece, o maior tesouro do mundo. Junte-se a Luffy e sua tripulação enquanto eles navegam pelos mares, enfrentam inimigos poderosos e descobrem os mistérios do Grand Line. One Piece é uma aventura emocionante que celebra a amizade, a liberdade e a busca por sonhos.",
+          link: "https://pt.wikipedia.org/wiki/One_Piece",
+          principaisPersonagens: "Monkey D. Luffy, Roronoa Zoro, Nami, Usopp, Sanji",
+          criador: "Eiichiro Oda",
+          genero: "Aventura, Shounen",
+          anoLancamento: "1999",
+          estudio: "Toei Animation",
+          numEpisodios: "1000+",
+          imagem: "https://example.com/one_piece.jpg"
+        },
+        {
+          titulo: "Attack on Titan",
+          descricao: "Em um mundo onde a humanidade vive dentro de muralhas para se proteger de titãs gigantes, Eren Yeager jura exterminar todos os titãs após presenciar a morte de sua mãe. Acompanhe a jornada de Eren e seus amigos enquanto eles desvendam os mistérios por trás dos titãs e lutam pela liberdade da humanidade.",
+          link: "https://pt.wikipedia.org/wiki/Attack_on_Titan",
+          principaisPersonagens: "Eren Yeager, Mikasa Ackerman, Armin Arlert",
+          criador: "Hajime Isayama",
+          genero: "Ação, Fantasia, Terror",
+          anoLancamento: "2013",
+          estudio: "Wit Studio",
+          numEpisodios: "87",
+          imagem: "https://example.com/attack_on_titan.jpg"
+        },
+        {
+            titulo: "Death Note",
+            descricao: "Light Yagami, um estudante do ensino médio, encontra um misterioso caderno com o poder de matar qualquer pessoa cujo nome seja escrito nele. Obcecado com a ideia de criar um mundo sem crimes, Light adota a identidade de Kira e começa a eliminar criminosos. Acompanhe a batalha intelectual entre Light e o detetive L.",
+            link: "https://pt.wikipedia.org/wiki/Death Note",
+            principaisPersonagens: "Light Yagami, L, Ryuk",
+            criador: "Tsugumi Ohba e Takeshi Obata",
+            genero: "Suspense, Psicologia, Sobrenatural",
+            anoLancamento: "2006",
+            estudio: "Madhouse",
+            numEpisodios: "37",
+            imagem: "https://example.com/death_note.jpg"
+          },
+          {
+            titulo: "Fullmetal Alchemist: Brotherhood",
+            descricao: "Os irmãos Edward e Alphonse Elric tentam trazer sua mãe de volta à vida através da alquimia, mas pagam um preço alto por sua ambição. Acompanhe a jornada dos irmãos Elric enquanto eles buscam a Pedra Filosofal para restaurar seus corpos e reparar os erros do passado.",
+            link: "https://pt.wikipedia.org/wiki/Fullmetal_Alchemist:_Brotherhood",
+            principaisPersonagens: "Edward Elric, Alphonse Elric, Roy Mustang, Riza Hawkeye",
+            criador: "Hiromu Arakawa",
+            genero: "Aventura, Fantasia, Drama",
+            anoLancamento: "2009",
+            estudio: "Bones",
+            numEpisodios: "64",
+            imagem: "https://example.com/fullmetal_alchemist.jpg"
+          },
+          {
+            titulo: "My Hero Academia",
+            descricao: "Em um mundo onde a maioria das pessoas possui superpoderes, Izuku Midoriya, um garoto sem poderes, sonha em se tornar um herói. Após um encontro com o maior herói, All Might, Izuku recebe um poder incrível e ingressa na prestigiada escola de heróis U.A. High School.",
+            link: "https://pt.wikipedia.org/wiki/My_Hero_Academia",
+            principaisPersonagens: "Izuku Midoriya, Katsuki Bakugo, Ochaco Uraraka, All Might",
+            criador: "Kohei Horikoshi",
+            genero: "Ação, Shounen",
+            anoLancamento: "2016",
+            estudio: "Bones",
+            numEpisodios: "148",
+            imagem: "https://example.com/my_hero_academia.jpg"
+          },
+          {
+            titulo: "Cowboy Bebop",
+            descricao: "Em um futuro próximo, a equipe do Bebop, uma nave espacial, caça recompensas para sobreviver. Acompanhe as aventuras de Spike Spiegel, Faye Valentine, Jet Black, Ed e Ein enquanto eles viajam pelo espaço, enfrentam diversos tipos de criminosos e lidam com seus próprios problemas pessoais.",
+            link: "https://pt.wikipedia.org/wiki/Cowboy_Bebop_(anime)",
+            principaisPersonagens: "Spike Spiegel, Faye Valentine, Jet Black",
+            criador: "Shinichiro Watanabe",
+            genero: "Sci-Fi, Aventura, Neo-Noir",
+            anoLancamento: 1998,
+            estudio: "Sunrise",
+            numEpisodios: 26,
+            imagem: "https://example.com/cowboy_bebop.jpg"
+        },
+        {
+            titulo: "Neon Genesis Evangelion",
+            descricao: "Em um futuro distópico, a humanidade enfrenta a ameaça de criaturas gigantescas conhecidas como Anjos. Shinji Ikari, um adolescente introvertido, é recrutado para pilotar um Evangelion, uma máquina gigante capaz de combater os Anjos. Acompanhe a jornada de Shinji enquanto ele lida com seus traumas e o destino da humanidade.",
+            link: "https://pt.wikipedia.org/wiki/Neon_Genesis_Evangelion",
+            principaisPersonagens: "Shinji Ikari, Rei Ayanami, Asuka Langley Soryu",
+            criador: "Hideaki Anno",
+            genero: "Mecha, Psicologia, Sci-Fi",
+            anoLancamento: "1995",
+            estudio: "Gainax",
+            numEpisodios: "26",
+            imagem: "https://example.com/neon_genesis_evangelion.jpg"
+        },
+        {
+            titulo: "Code Geass: Lelouch of the Rebellion",
+            descricao: "Lelouch vi Britannia, um príncipe exilado, encontra uma poderosa arma que lhe permite controlar as mentes das pessoas. Sob a identidade de Zero, ele lidera uma rebelião contra o Império Britânico, com o objetivo de criar um mundo onde sua irmã possa viver livremente.",
+            link: "https://pt.wikipedia.org/wiki/Code_Geass",
+            principaisPersonagens: "Lelouch vi Britannia, Suzaku Kururugi, C.C.",
+            criador: "Sunrise",
+            genero: "Mecha, Sci-Fi, Drama",
+            anoLancamento: "2006",
+            estudio: "Sunrise",
+            numEpisodios: "50",
+            imagem: "https://example.com/code_geass.jpg"
+        },
+        {
+            titulo: "Mob Psycho 100",
+            descricao: "Shigeo Kageyama, um adolescente com habilidades psíquicas extraordinárias, luta para controlar seus poderes enquanto lida com os desafios da vida cotidiana e as ameaças sobrenaturais.",
+            link: "https://pt.wikipedia.org/wiki/Mob_Psycho_100",
+            principaisPersonagens: "Shigeo Kageyama (Mob), Reigen Arataka",
+            criador: "ONE",
+            genero:  "Comédia, Sobrenatural, Ação",
+            anoLancamento: "2016",
+            estudio: "Bones",
+            numEpisodios: "25",
+            imagem: "https://example.com/mob_psycho_100.jpg"
+        }
+      ];
+
+
+
